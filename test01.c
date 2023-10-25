1f5b673c557f0f6f6eb7c990c727571a97576fda
molofu <484023027@qq.com>
2023-10-25 16:21:51 +0800

    小端存储

---
+++ test01.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+union 
+{
+    int a;
+    char b;
+}un;
+
+int main()
+{
+
+
+    un.a=0x12345678;
+
+    if(un.b == 0x78)
+    {
+        printf("小端存储\n");
+    }
+    else
+    {
+        printf("大端存储\n");
+    }
+
+
+    return 0;
+}
