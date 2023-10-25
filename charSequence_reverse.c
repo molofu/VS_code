f9cc7b5455ec3c46ff280b34615b0bbcfd809f17
molofu <484023027@qq.com>
2023-10-25 18:35:20 +0800

    字节序转换函数

---
+++ charSequence_reverse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 0x12345678;
+    short b = 0x1234;
+
+    printf("%#x\n",htonl(a));               //%x 16进制
+    printf("%#x\n",htons(b));               //%#x 16进制 前加0x
+
+
+    return 0;
+}
